db812cd65fb22fce3f93d2200b1309f05f51a36d
Tamas <jonastamas2@gmail.com>
2019-08-11 14:18:45 +0100

    Refactor getScore

--- index.js
+++ index.js
@@ -11,34 +11,34 @@ class Game {
     }
   };
 
+  isSpare = rollIndex => (this.getRollScore(rollIndex) + this.getRollScore(rollIndex + 1) === 10);
+
+  isSpin = rollIndex => (this.getRollScore(rollIndex) === 10);
+
   getRollScore = rollIndex => (this.rolls[rollIndex] || 0);
 
   getSpinScore = rollIndex => (10 + this.getRollScore(rollIndex + 2) + this.getRollScore(rollIndex + 3));
 
   getSpareScore = rollIndex => (10 + this.getRollScore(rollIndex + 2));
 
-  isSpare = rollIndex => (this.getRollScore(rollIndex) + this.getRollScore(rollIndex + 1) === 10);
+  getFrameScore = (rollIndex) => {
+    if (this.isSpin(rollIndex)) {
+      return this.getSpinScore(rollIndex);
+    }
 
-  isSpin = rollIndex => (this.getRollScore(rollIndex) === 10);
+    if (this.isSpare(rollIndex)) {
+      return this.getSpareScore(rollIndex);
+    }
+
+    return this.getRollScore(rollIndex) + this.getRollScore(rollIndex + 1);
+  };
 
   getScore = () => {
     let score = 0;
     let rollIndex = 0;
 
     for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
-      if (this.isSpin(rollIndex)) {
-
-        score += this.getSpinScore(rollIndex);
-
-      } else if (this.isSpare(rollIndex)) {
-
-        score += this.getSpareScore(rollIndex);
-
-      } else {
-
-        score += this.getRollScore(rollIndex) + this.getRollScore(rollIndex + 1);
-      }
-
+      score += this.getFrameScore(rollIndex);
       rollIndex += 2;
     }
 
