7849cead3d200e466d46c86c286981f699d48cee
Tamas <tamas@giffgaff.co.uk>
2019-08-12 13:50:21 +0100

    Test perfect game

--- index.js
+++ index.js
@@ -5,25 +5,21 @@ class Game {
 
   roll = (pins) => {
     this.rolls.push(pins);
-
-    if (pins === 10) {
-      this.rolls.push(0);
-    }
   };
 
   isSpare = rollIndex => (this.getRollScore(rollIndex) + this.getRollScore(rollIndex + 1) === 10);
 
-  isSpin = rollIndex => (this.getRollScore(rollIndex) === 10);
+  isStrike = rollIndex => (this.getRollScore(rollIndex) === 10);
 
   getRollScore = rollIndex => (this.rolls[rollIndex] || 0);
 
-  getSpinScore = rollIndex => (10 + this.getRollScore(rollIndex + 2) + this.getRollScore(rollIndex + 3));
+  getStrikeScore = rollIndex => (10 + this.getRollScore(rollIndex + 1) + this.getRollScore(rollIndex + 2));
 
   getSpareScore = rollIndex => (10 + this.getRollScore(rollIndex + 2));
 
   getFrameScore = (rollIndex) => {
-    if (this.isSpin(rollIndex)) {
-      return this.getSpinScore(rollIndex);
+    if (this.isStrike(rollIndex)) {
+      return this.getStrikeScore(rollIndex);
     }
 
     if (this.isSpare(rollIndex)) {
@@ -39,7 +35,7 @@ class Game {
 
     for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
       score += this.getFrameScore(rollIndex);
-      rollIndex += 2;
+      rollIndex += this.isStrike(rollIndex) ? 1 : 2;
     }
 
     return score;
--- index.test.js
+++ index.test.js
@@ -18,8 +18,7 @@ describe('Game', () => {
   });
 
   it('returns score 16 if roll a spare and 3', () => {
-    game.roll(5);
-    game.roll(5);
+    rollSpare();
     game.roll(3);
 
     expect(game.getScore()).toEqual(16);
@@ -28,19 +27,39 @@ describe('Game', () => {
   it('returns score 18 if roll 1 1 and a spare and 3', () => {
     game.roll(1);
     game.roll(1);
-    game.roll(5);
-    game.roll(5);
+    rollSpare();
     game.roll(3);
 
     expect(game.getScore()).toEqual(18);
   });
 
-  it('returns score 14 if roll a stike and 1 1', () => {
-    game.roll(10);
+  it('returns score 14 if roll a strike and 1 1', () => {
+    rollStrike(1);
     game.roll(1);
     game.roll(1);
 
     expect(game.getScore()).toEqual(14);
   });
 
+  it('returns score 60 if roll 3 strikes', () => {
+    rollStrike(3);
+
+    expect(game.getScore()).toEqual(60);
+  });
+
+  it('returns score 300 if roll perfect game', () => {
+    rollStrike(12);
+    expect(game.getScore()).toEqual(300);
+  });
 });
+
+function rollSpare() {
+  game.roll(5);
+  game.roll(5);
+};
+
+function rollStrike(n) {
+  for (let i = 0; i < n; i++) {
+    game.roll(10);
+  };
+}
